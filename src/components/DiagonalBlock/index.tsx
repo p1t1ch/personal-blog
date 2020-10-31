669b113d6d0952482744672d41befcaaf8f5ef2d
p1t1ch <belobeev.kirill@gmail.com>
2020-10-31 18:05:26 +0300

    simplify clipping algorithm

--- src/components/DiagonalBlock/index.tsx
+++ src/components/DiagonalBlock/index.tsx
@@ -6,8 +6,6 @@ import colorVar from '@/utils/colorVar'
 const Block = styled.div(
   ({ theme, topLine, bottomLine }: ThemeProps & { topLine: boolean; bottomLine: boolean; as?: string }) => ({
     position: 'relative',
-    ...(topLine ? { paddingTop: `calc(${theme.sizes.clipSize} - 1px)` } : {}),
-    ...(bottomLine ? { paddingBottom: `calc(${theme.sizes.clipSize} - 1px)` } : {}),
     '::after, ::before': {
       position: 'absolute',
       left: 0,
@@ -20,7 +18,7 @@ const Block = styled.div(
       ? {
           '::before': {
             content: '""',
-            top: 0,
+            top: 1,
           },
         }
       : {}),
@@ -28,7 +26,7 @@ const Block = styled.div(
       ? {
           '::after': {
             content: '""',
-            bottom: 0,
+            bottom: 1,
           },
         }
       : {}),
@@ -37,11 +35,11 @@ const Block = styled.div(
 
 const Content = styled.div(
   ({ theme, topLine, bottomLine }: ThemeProps & { topLine: boolean; bottomLine: boolean }) => ({
-    ...(topLine ? { marginTop: `calc(-${theme.sizes.clipSize} + ${theme.sizes.linesWidth})` } : {}),
-    ...(bottomLine ? { marginBottom: `calc(-${theme.sizes.clipSize} + ${theme.sizes.linesWidth})` } : {}),
-    clipPath: `polygon(0 0, 100% ${
-      topLine ? `calc(${theme.sizes.clipSize} - ${theme.sizes.linesWidth})` : '0'
-    }, 100% 100%, 0 ${bottomLine ? `calc(100% - ${theme.sizes.clipSize} + ${theme.sizes.linesWidth})` : '100%'})`,
+    position: 'relative',
+    zIndex: -1,
+    clipPath: `polygon(0 ${theme.sizes.linesWidth}, 100% ${topLine ? theme.sizes.clipSize : '0'}, 100% calc(100% - ${
+      theme.sizes.linesWidth
+    }), 0 ${bottomLine ? `calc(100% - ${theme.sizes.clipSize})` : '100%'})`,
     textAlign: 'center',
   })
 )
